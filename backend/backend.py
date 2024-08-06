aa0058f13d0462b8ccf97d3e7d7fc6507e3f851c
Christian Brzeski <cbrzeski@wks-225.speedport.ip>
2024-08-06 11:20:52 +0200

    New locations are saved persitently in locations.json in backend

--- backend/backend.py
+++ backend/backend.py
@@ -1,16 +1,18 @@
 #!/bin/python
 
-from flask import Flask, request, send_file, Response
+from flask import Flask, request, Response, jsonify
+from flask_cors import CORS
 import requests
+import os
 import json
-import gzip
-from io import BytesIO
 
 
 api_key = "Insert-API-key"
+project_root_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 app = Flask(__name__)
+CORS(app)
 
 def get_coordinate(zCoordinate, xCoordinate, yCoordinate):
     print(api_key)
@@ -20,19 +22,46 @@ def get_coordinate(zCoordinate, xCoordinate, yCoordinate):
     
     return Response(response.content, mimetype='image/png')
 
+@app.route("/getLocations", methods=['GET'])
+def getLocations():
+    #Read existing location entries from json array
+    with open(project_root_dir + "/locations.json", "r") as file:
+        locations = json.load(file)
+        file.close()
+
+    return locations
+
+@app.route("/saveLocation", methods=['POST'])
+def receive_data():
+    #Read existing location entries from json array
+    with open(project_root_dir + "/locations.json", "r") as file:
+        locations = json.load(file)
+        file.close()
+
+    #Ensure location data is a list
+    if not isinstance(locations, list):
+        raise ValueError("JSON data is not an array")
+
+    #Add new location to list
+    new_location = request.get_json()
+    locations.append(new_location)
+
+    #Write updated locations into locations.json
+    with open(project_root_dir + "/locations.json", "w") as file:
+        print("Writing into file...")
+        json.dump(locations, file, indent=4)
+        file.close()
+
+    return jsonify({"status": "success", "data_received": locations}), 200
+
 @app.route("/getcoordinate")
 def index():
     zCoordinate = request.args['zCoordinate']
     xCoordinate = request.args['xCoordinate']
     yCoordinate = request.args['yCoordinate']
 
-    data = {
-        "zCoordinate": zCoordinate,
-        "xCoordinate": xCoordinate,
-        "yCoordinate": yCoordinate
-    }
-    
     #return data
     return get_coordinate(zCoordinate, xCoordinate, yCoordinate)
+
 
 app.run(host="0.0.0.0", port=5000)
--- main.js
+++ main.js
@@ -2,78 +2,7 @@
 const defaultImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/No_image_3x4.svg/1200px-No_image_3x4.svg.png";
 const defaultCity = "Hamburg";
 const defaultDistrict = "Hamburg";
-const locations = [{
-    name: "Rothenburgsort",
-    city: "Hamburg",
-    district: "Rothenburgsort",
-    geolocation: {
-        lat: 53.527997,
-        lng: 10.041561
-    },
-    img: "https://lh5.googleusercontent.com/p/AF1QipNEw_rHamcGoTo0WHYxsfbf_8Rx2HVSaKF_UqtF=w408-h272-k-no",
-    id: 1,
-    street:"default",
-    houseNumber:"default",
-    country: "DE"
-},
-{
-    name: "Lighthouse Zero",
-    city: "Hamburg",
-    district: "Hafencity",
-    geolocation: {
-        lat: 53.538453, 
-        lng: 10.003782
-        },
-    img: "https://lh5.googleusercontent.com/p/AF1QipNEw_rHamcGoTo0WHYxsfbf_8Rx2HVSaKF_UqtF=w408-h272-k-no",
-    id: 2,
-    street:"default",
-    houseNumber:"default",
-    country: "DE"
-},
-{
-    name: "Müllberg Hummelsbüttel",
-    city: "Hamburg",
-    district: "Hummelsbüttel",
-    geolocation: {
-        lat: 53.674082221321115,
-        lng: 10.050298561390438
-    },
-    img: "https://lh5.googleusercontent.com/p/AF1QipNEw_rHamcGoTo0WHYxsfbf_8Rx2HVSaKF_UqtF=w408-h272-k-no",
-    id: 3,
-    street:"default",
-    houseNumber:"default",
-    country: "DE"  
-},
-{
-    name: "Altonaer Balkon Aussichtspunkt",
-    city: "Hamburg",
-    district: "Altona",
-    geolocation: {
-        lat: 53.545243,
-        lng: 9.915153
-    },
-    img: "https://lh5.googleusercontent.com/p/AF1QipNEw_rHamcGoTo0WHYxsfbf_8Rx2HVSaKF_UqtF=w408-h272-k-no",
-    id: 4,
-    street:"default",
-    houseNumber:"default",
-    country: "DE"  
-},
-{
-    name: "Stadtpark Eichhörnchen",
-    city: "Cologne",
-    district: "Winterhude",
-    geolocation: {
-        lat: 53.59843584184574,
-        lng: 10.023464113402172
-    },
-    img: "https://lh5.googleusercontent.com/p/AF1QipNEw_rHamcGoTo0WHYxsfbf_8Rx2HVSaKF_UqtF=w408-h272-k-no",
-    id: 5,
-    street:"default",
-    houseNumber:"default",
-    country: "DE"
-},
-]
-
+var markers = [];
 
 const btnCloseForm = document.querySelector("#btn-close-form");
 const form = document.querySelector("#form");
@@ -124,7 +53,7 @@ formSubmit.addEventListener("click", function(event){
     locations.push(newSpot);
 
     console.log(locations)
-    saveLocation(newSpot);
+    saveLocation(newSpot, locations);
     
     //TODO: Fix this
     createNavItem(newSpot);
@@ -151,49 +80,6 @@ formSubmit.addEventListener("click", function(event){
 //Initialize Map
 let map = L.map('map').setView([53.55, 9.993], 13);
 
-//define fullscreen control button
-
-let customControl =  L.Control.extend({        
-    options: {
-      position: 'topright'
-    },
-
-    onAdd: function (map) {
-        // create the control container to open the formContainer
-      let openFormContainer = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom leaflet-control-add-marker');
-      openFormContainer.appendChild(L.DomUtil.create('i', 'fas fa-plus', openFormContainer));
-      openFormContainer.onclick = function(event){
-        event.preventDefault();
-        event.stopPropagation();
-        toggleForm();
-        
-      }
-
-      return openFormContainer;
-    }
-});
-let navControl =  L.Control.extend({
-    options: {
-        position: 'topright'
-    },
-    onAdd: function (map) {
-        // create the control container to open the formContainer
-        let openNav = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom leaflet-control-open-nav');
-        openNav.appendChild(L.DomUtil.create('i', 'fas fa-bars', openNav));
-        openNav.onclick = function(event){
-            event.preventDefault();
-            event.stopPropagation();
-            toggleNav();
-        }
-        
-        return openNav;
-    }
-});
-
-
-map.addControl(new customControl());
-map.addControl(new navControl());
-
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -215,11 +101,57 @@ function(e){
     navigator.clipboard.writeText(text).then(function() {
      console.log('Async: Copying "'+text +'to clipboard was successful!');
      showCopyNotification();
-}, 
-function(err) {
-  console.error('Async: Could not copy text: ', err);
-});
+    }, 
+    function(err) {
+    console.error('Async: Could not copy text: ', err);
     });
+});
+
+//define fullscreen control button
+
+let customControl =  L.Control.extend({        
+    options: {
+      position: 'topright'
+    },
+
+    onAdd: function (map) {
+        // create the control container to open the formContainer
+      let openFormContainer = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom leaflet-control-add-marker');
+      openFormContainer.appendChild(L.DomUtil.create('i', 'fas fa-plus', openFormContainer));
+      openFormContainer.onclick = function(event){
+        event.preventDefault();
+        event.stopPropagation();
+        toggleForm();
+        
+      }
+
+      return openFormContainer;
+    }
+});
+
+
+let navControl =  L.Control.extend({
+    options: {
+        position: 'topright'
+    },
+    onAdd: function (map) {
+        // create the control container to open the formContainer
+        let openNav = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom leaflet-control-open-nav');
+        openNav.appendChild(L.DomUtil.create('i', 'fas fa-bars', openNav));
+        openNav.onclick = function(event){
+            event.preventDefault();
+            event.stopPropagation();
+            toggleNav();
+        }
+        
+        return openNav;
+    }
+});
+
+map.addControl(new customControl());
+map.addControl(new navControl());
+
+
 function showCopyNotification() {
     console.log("show notification");
     let notification = document.querySelector(".notification");
@@ -230,53 +162,101 @@ function showCopyNotification() {
 }
 
 
+async function updateMap(locations) {
+    locations.forEach(location => {
+        //setGeoData(location);
+        setMarkerOnMap(location);
+    });
+}
 
-let markers = [];
 
-locations.forEach((location)=>{
-    setGeoData(location);
-    saveLocation(location);
-});
-async function saveLocation(location){
+
+async function getLocations(){
+    const response = await fetch('http://127.0.0.1:5000/getLocations', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    if(!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const response_json = await response.json();
+    return response_json;
+    // .then(response => response.json())
+    // .then(data => {
+    //     console.log('Success:', data);
+    // })
+    // .catch((error) => {
+    //     console.error('Error:', error);
+    // });
+}
+
+async function saveLocation(location, locations){
     //wait for fethced data then call setStreet
 
+    //save location in list
+    fetch('http://127.0.0.1:5000/saveLocation', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(location)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 
-    let marker = L.marker(location.geolocation).addTo(map);
-    marker._icon.id = location.id;
-    let popup = L.popup()
-    
-    .setLatLng(location.geolocation)
-    .setContent(
-    `<h2 class="popup-headline">${location.name}</h2>
-    <p class="popup-desc">District: ${location.district}<br>
-    City: ${location.city}<br>
-    Longitude: ${location.geolocation.lng}<br>
-    Latitude: ${location.geolocation.lat}</p>
-    Address: ${location.street}<br>
-    Img: ${location.img} <br>
-    <img src="${location.img}" class="popup-img" alt="Image of ${location.name}">
-    <div class="google-link"><a target="_blank" href="https://www.google.de/maps/place/${location.geolocation.lat},${location.geolocation.lng}">Google Maps</a></div>
-    `)
-    
-    marker.bindPopup(popup);
-    markers.push(marker); 
+    updateMap(locations)
 }
- async function setGeoData(location) {
-    fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${location.geolocation.lat}&lon=${location.geolocation.lng}`)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    })
+//  function setGeoData(location) {
+//     console.log("Location: " + location)
+//     fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${location.geolocation.lat}&lon=${location.geolocation.lng}`)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(data) {
+//         console.log(data);
+//     })
  
+// }
+
+function setMarkerOnMap(location) {
+     //Set marker on map
+     let marker = L.marker(location.geolocation).addTo(map);
+     marker._icon.id = location.id;
+     let popup = L.popup()
+     
+     .setLatLng(location.geolocation)
+     .setContent(
+     `<h2 class="popup-headline">${location.name}</h2>
+     <p class="popup-desc">District: ${location.district}<br>
+     City: ${location.city}<br>
+     Longitude: ${location.geolocation.lng}<br>
+     Latitude: ${location.geolocation.lat}</p>
+     Address: ${location.street}<br>
+     Img: ${location.img} <br>
+     <img src="${location.img}" class="popup-img" alt="Image of ${location.name}">
+     <div class="google-link"><a target="_blank" href="https://www.google.de/maps/place/${location.geolocation.lat},${location.geolocation.lng}">Google Maps</a></div>
+     `)
+     
+     marker.bindPopup(popup);
+     markers.push(marker); 
 }
+
+
 
 //load nav items
 const nav = document.querySelector("#nav-list");
 const navHeaders = document.querySelector("#nav-headers");
 const navCity = document.querySelector("#nav-city");
-locations.forEach(CreateNavCity);
+
+
 //function that creates entry with city in nav if not already there
 async function CreateNavCity(location){
     if(!navHeaders.innerHTML.includes(location.city)){
@@ -290,6 +270,7 @@ async function CreateNavCity(location){
     createNavItem(location);
   
 }
+
 function createNavItem(location){
     let navContainer = document.querySelector(`.dropdown-content`);
     navContainer.innerHTML += `<a href="#${location.name}">${location.name}</a>`
@@ -322,7 +303,7 @@ function toggleNav(){
     
 }
 
-//Open Form via Add Sot button
+//Open Form via Add Spot button
 const btnOpenForm = document.querySelector("#btn-open-form");
 btnOpenForm.addEventListener("click", function(event){
     event.preventDefault();
@@ -336,6 +317,19 @@ function toggleForm(){
         formContainer.classList.add("hidden");
     }
 }
+
+async function initializeMap()  {
+    locations = await getLocations();
+    updateMap(locations);
+    locations.forEach(CreateNavCity);
+} 
+    
+
+//Setup map on page load
+window.onload = (event) => { 
+    initializeMap()
+};
+
 
 
 
